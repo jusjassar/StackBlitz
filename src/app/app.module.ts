96674e9e861b1521d3dd2025e5f2436d691eb9e5
jusjassar <117742554+jusjassar@users.noreply.github.com>
2023-07-27 19:30:59 -0400

    2 files modified, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { StudentsComponent } from './students/students.component';
-
-const appRoutes:Routes=[
-{path:'students',component:StudentsComponent}
-]
 
 @NgModule({
-  imports: [BrowserModule,
-  RouterModule.forRoot(appRoutes),
-HttpClientModule],
-  declarations: [
-    AppComponent,
-    StudentsComponent
-  ],
+  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
